32e65e56886b3ad1204468ad6a99f8e43c72b7ad
KangChan Ko <bomedame97@gmail.com>
2023-01-06 11:31:56 +0900

    01st commit

---
+++ Chapter02/src/chapter2/DoubleEx.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class DoubleEx {
+
+	public static void main(String[] args) {
+		/*var dNum1 = 3.14; //지역 변수 자료형 추론
+		double dNum2 = 3.14;
+		float fNum = 3.14F; //실수는 기본적으로 더블로 저장되서 플로트로 저장하려면 뒤에 F를 붙여줘야된다.
+		
+		System.out.println(dNum1);
+		
+		var dNum3 = 10;
+		System.out.println(dNum3);
+		
+		num = 3.14; 이런식으로 중간에 바뀌는게 안된다.*/
+	}
+}
---
+++ Chapter02/src/chapter2/VariableEx.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+
+		int num;
+		num = 10; //¿¤º§ ¾Ëº§ left value, right value
+		
+		System.out.println(num);
+		
+		int level = 20;
+		System.out.println(level);
+		
+		int ns;
+		int numberOfStudent;
+	}
+
+}
